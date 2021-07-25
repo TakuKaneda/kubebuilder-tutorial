5989710d6fbe1a56adf28de90710ca40241166ba
TakuKaneda <kndtaku@gmail.com>
2021-07-25 16:33:16 +0900

    Implement Reconcile

--- main.go
+++ main.go
@@ -80,6 +80,7 @@ func main() {
 
 	if err = (&controllers.CronJobReconciler{
 		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("CronJob"),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "CronJob")
